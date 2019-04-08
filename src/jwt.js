ddd784e466e051d33f83dec318a9048b5b95c7ce
tomon9086 <tomo.mouse.9086@gmail.com>
2019-04-08 15:50:07 +0900

    アルゴリズムをHS256にしてみる

--- src/jwt.js
+++ src/jwt.js
@@ -1,21 +1,24 @@
 const jwt = require("jsonwebtoken")
 
-const path = require("path")
 const promisify = require("util").promisify
 const fs = require("fs")
 const readFile = promisify(fs.readFile)
 
 module.exports = {
 	encrypt: (data, key_path) => {
+		if(typeof key_path !== "string")
+			throw new Error("key_path must be a string")
 		return new Promise(async (resolve, reject) => {
-			const privateKey = await readFile(path.resolve(__dirname, key_path))
-			resolve(jwt.sign(data, privateKey, { algorithm: "RS256"}))
+			const key = await readFile(key_path)
+			resolve(jwt.sign(data, key, { algorithm: "HS256"}))
 		})
 	},
 	decrypt: (token, key_path) => {
+		if(typeof key_path !== "string")
+			throw new Error("key_path must be a string")
 		return new Promise(async (resolve, reject) => {
-			const publicKey = await readFile(path.resolve(__dirname, key_path))
-			jwt.verify(token, publicKey, { algorithms: ["RS256"] }, function(err, payload) {
+			const cert = await readFile(key_path)
+			jwt.verify(token, cert, { algorithm: "HS256" }, function(err, payload) {
 				if(err)
 					reject(err)
 				else
